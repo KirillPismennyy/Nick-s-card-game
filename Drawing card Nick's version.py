8f061981bd8c08f4444097bb48e4a3ec518e9db8
Ник Valov <nikvalov@MacBook-Air-Nik.localdomain>
2023-01-31 16:38:37 +0200

    test

--- Drawing card Nick's version.py
+++ Drawing card Nick's version.py
@@ -8,7 +8,7 @@ im = Image.new("RGB", (width,height))
 im.save("Image1.png")
 
 d = ImageDraw.Draw(im)
-#fnt = ImageFont.truetype('arial.ttf', 50)
+fnt = ImageFont.truetype('NewYork.ttf', 50)
 #get
 d.text((0.75*width, 0.15*height),monsterName, fill=(255,255,55))
 e = ImageDraw.Draw(im)
@@ -17,4 +17,3 @@ f = ImageDraw.Draw(im)
 f.text((0.78*width, 0.14*height),(monsterHealthInsurance), fill=(255,255,65))
 
 im.show()
-
